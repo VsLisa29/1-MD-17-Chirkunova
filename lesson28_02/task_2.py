31ea266ec8f979c7412c3a18177c4ecf7e3990d6
VsLisa29 <159264420+VsLisa29@users.noreply.github.com>
2024-03-06 16:30:04 +0300

    Update task_2.py

--- lesson28_02/task_2.py
+++ lesson28_02/task_2.py
@@ -1,6 +1,8 @@
 result = ""
 while True:
     a = input("Введите слово, для окончания введите стоп ")
-    result = result + a + " "
-if a == "стоп":
-    print(result)
+    if a == "стоп":
+        print(result)
+        break
+    else:
+        result = result + a + " "
