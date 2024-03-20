455d244ddbd3f1c85142e698b56fbb7c9315471e
VsLisa29 <anastasia.chirkunova@mail.ru>
2024-03-20 16:40:38 +0300

    lesson20_03

---
+++ lesson20_03/task_1.py
@@ -0,0 +1,45 @@
+def proverka_del():
+    number = int(input("Введите число "))
+    if number % 7 == 0:
+        print(f"{number} делится на 7")
+    else:
+        print(f"{number} не делится на 7")
+
+def del_200():
+    try:
+        user_input = input("Введите число для деления 200 ")
+        number = float(user_input)
+        result = 200 / number
+        print(f"Результат деления 200 на {number}: {result}")
+    except ValueError:
+        print("Ошибка: Введено не число. Пожалуйста, введите число.")
+    except ZeroDivisionError:
+        print("Ошибка: Деление на ноль. Пожалуйста, введите число отличное от нуля.")
+
+def magic_date():
+    user_date = input("Введите дату в формате ДД.ММ.ГГГГ ")
+    day, month, year = map(int, user_date.split('.') )
+
+    if day * month == year % 100:
+        print(f"{user_date} Дата является магической")
+    else:
+        print(f"{user_date} Это обычная дата")
+
+def lucky_ticket():
+    ticket_number = int(input("Введите номер билета "))
+
+    if len(str(ticket_number)) % 2 != 0:
+        return False
+
+    part_len = len(str(ticket_number)) // 2
+    first_part = ticket_number
+    second_part = ticket_number
+    #sum_first_part =
+    #sum_second_part =
+
+
+    if lucky_ticket(ticket_number):
+        print(f"{ticket_number} Этот билет счастливый")
+    else:
+        print(f"{ticket_number} Этот билет обычный")
+
